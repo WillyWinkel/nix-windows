110a845c394651f27f176ab2255642b979a8980d
lennard <lennard.pfennig@bruker.com>
2025-05-31 15:17:10 +0200

    fix sha

--- home.nix
+++ home.nix
@@ -3,7 +3,7 @@
 let
   nixpkgs-25_05 = import (builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/25.05.tar.gz";
-    sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="; # Replace with actual sha256
+    sha256 = "sha256-1915r28xc4znrh2vf4rrjnxldw2imysz819gzhk9qlrkqanmfsxd";
   }) {};
 in
 {
