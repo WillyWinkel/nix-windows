66207e141c4f19bc6c9614e95eae7e52c8072c29
lennard <lennard.pfennig@bruker.com>
2025-05-31 13:21:07 +0200

    trying default shell stuff

--- home.nix
+++ home.nix
@@ -1,76 +1,59 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "willy";
   home.homeDirectory = "/home/willy";
+  home.stateVersion = "25.05";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "25.05"; # Please read the comment before changing.
-
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+    pkgs.hello
+    (pkgs.writeShellScriptBin "my-hello" ''
+      echo "Hello, ${config.home.username}!"
+    '')
+    pkgs.fish
+    pkgs.tide
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
+    # Example:
     # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
     #   org.gradle.console=verbose
     #   org.gradle.daemon.idletimeout=3600000
     # '';
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/willy/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    EDITOR = "vim";
   };
 
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  programs.fish = {
+    enable = true;
+    plugins = [
+      {
+        name = "tide";
+        src = pkgs.tide;
+      }
+    ];
+  };
+
+  home.activation.tideConfigure = pkgs.lib.hm.dag.entryAfter ["writeBoundary"] ''
+    ${pkgs.fish}/bin/fish -c 'tide configure --auto --style=Classic --prompt_colors=16 --show_time=No --lean_prompt=No --prompt_connection=Round --prompt_spacing=Compact --icons=Unicode --transient=Yes'
+  '';
+
+  home.activation.setFishDefaultShell = pkgs.lib.hm.dag.entryAfter ["tideConfigure"] ''
+    fish_bin="$(command -v fish)"
+    if [ -n "$fish_bin" ]; then
+      if ! grep -qx "$fish_bin" /etc/shells; then
+        echo "$fish_bin" | sudo tee -a /etc/shells >/dev/null
+      fi
+      if command -v chsh >/dev/null 2>&1; then
+        if [ "$(getent passwd "$USER" | cut -d: -f7)" != "$fish_bin" ]; then
+          chsh -s "$fish_bin" || true
+        fi
+      fi
+    fi
+  '';
 }
